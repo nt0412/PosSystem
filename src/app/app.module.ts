04fc8a9c67315c0c1ec43c0adc0aa8384ff8ec2c
nt0412 <namthuan412@gmail.com>
2022-05-20 12:06:15 +0700

    add sidebar and install some module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
